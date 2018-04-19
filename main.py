e273437ec288a7a84c5344df02f35bf435ed7dc2
anggakharisma <anggaputrakharisma@gmail.com>
2018-04-19 07:49:52 +0700

    Change driver location depends on OS

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import re
 import webscraper
 from flask import Flask, jsonify, request, redirect
 from flask_cors import CORS
+import os
 
 urlToped = "https://www.tokopedia.com/search?st=product&q="
 urlBukaLapak = "https://www.bukalapak.com/products?utf8=✓&source=navbar&from=omnisearch&search_source=omnisearch_organic&search[hashtag]=&search[keywords]="
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
-driver = webdriver.Chrome(executable_path=r"C:\Libs\chromedriver.exe", chrome_options=options)
+
+driver = webdriver.Chrome(chrome_options=options)
+if(os == 'nt'):
+    driver = webdriver.Chrome(executable_path=r"C:\Libs\chromedriver.exe", chrome_options=options)
+
 app = Flask(__name__)
 CORS(app)
 
